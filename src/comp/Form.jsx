12b7f60dbb70f870cb5c511660c036e2882c4d17
Ankit <rattanankit2004@gmail.com>
2023-12-30 00:36:18 +0530

    ui form

--- src/comp/Form.jsx
+++ src/comp/Form.jsx
@@ -25,12 +25,49 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
-      <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-      <textarea name="message" placeholder="Message" onChange={handleChange} required />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="bg-gray-900 p-5 flex items-center justify-center">
+      <form
+        className="bg-black p-4 rounded shadow-2xl hover:shadow-cyan-300 max-w-md w-full duration-1000"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl mb-4 text-sky-300 font-bold">Contact Me</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <textarea
+            name="message"
+            placeholder="Message"
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
