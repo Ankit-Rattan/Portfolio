a86e0e64750ac97fcbcffc6edb247b5f65cc7a21
Ankit <rattanankit2004@gmail.com>
2024-01-01 19:51:13 +0530

    update pic

--- frontend/src/comp/About.jsx
+++ frontend/src/comp/About.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react'
+import { motion, useTransform, useViewportScroll } from 'framer-motion'
+import Pic from './images/pic.jpeg'
 
 const About = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+     const timer = setTimeout(() => {
+       setVisible(true);
+     }, 2000);
+     return () => clearTimeout(timer);
+  }, []);
   return (
     <>
+    {visible &&
+      <motion.div initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 3 }}>
 
-    <div className='flex justify-around flex-wrap'>
-    <div className='border border-white border-2 p-2'>
 
-      <div className='text-white font-mono text-xl bg-blue-950 p-2 shadow-sm  shadow-slate-400'>
-        -Name : Ankit Rattan
-        <br/>
-        <br/>
-        -College: National Institute of Technology Delhi
-        <br/>
-        <br/>
-        -Batch: 2026
-        <br/>
-        <br/>
-        -Email Id: rattanankit2004@gmail.com 
-        <br/>
-        <br/>
-         -Love to Write and Create 
-        💻✍️
-        <br/>
+        <div className='flex justify-around flex-wrap items-center'>
+          <div>
 
-        
-      </div>
-    </div>
-      <div>
-        <img src=''/>
+            <div className='border-white border-2 p-2'>
 
-      </div>
-    </div>
+              <div className='text-white font-mono text-xl bg-blue-950 p-2 shadow-sm  shadow-slate-400'>
+                -Name : Ankit Rattan
+                <br />
+                <br />
+                -College: National Institute of Technology Delhi
+                <br />
+                <br />
+                -Batch: 2026
+                <br />
+                <br />
+                -Email Id: rattanankit2004@gmail.com
+                <br />
+                <br />
+                -Love to Write and Create
+                💻✍️
+                <br />
+
+
+              </div>
+            </div>
+          </div>
+          <div className='items-center'>
+            <img className='h-[25rem]  ' src={Pic} />
+
+          </div>
+        </div>
+      </motion.div>
+    }
     </>
   )
 }
