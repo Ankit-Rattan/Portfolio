6465869ea39dccd56bc8cdd551811024b1196b92
Ankit <rattanankit2004@gmail.com>
2024-01-14 02:17:51 +0530

    update

--- frontend/src/comp/Project.jsx
+++ frontend/src/comp/Project.jsx
@@ -25,10 +25,10 @@ const Project = () => {
 
 
                     <div className="mt-8 w-[30rem]">
-                        <h1 className="text-2xl font-semibold text-red-300">ChatApp</h1>
-                        <p className="mt-2 text-lime-200">MERN stack Project</p>
-                        <p className="mt-2 text-cyan-200">The common platform ChatApp .</p>
-                        <p className="mt-2 text-white">Tech Stack : Vite+Rect Tailwind CSS MongoAtlas Express Node BCrypt Socket</p>
+                        <h1 className="text-2xl font-semibold text-red-300">ThoughtBox</h1>
+                        <p className="mt-2 text-lime-200">MERN+Socket Project</p>
+                        <p className="mt-2 text-cyan-200">It is an open chat app for sharing your knowledge and thoughts with others, where you can also comment and any other person on their thoughts.</p>
+                        <p className="mt-2 text-white">Tech Stack : Vite+Rect Tailwind CSS MongoAtlas Express Node  Socket</p>
                         <a href="https://github.com/Ankit-Rattan/Chat_App-beta" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
                     </div>
 
