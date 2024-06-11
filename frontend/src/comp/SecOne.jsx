f0063cd7787cf72a04a0c835b71d6c75dbd9b7e8
Ankit <rattanankit2004@gmail.com>
2024-06-11 22:18:06 +0530

    update

--- frontend/src/comp/SecOne.jsx
+++ frontend/src/comp/SecOne.jsx
@@ -13,11 +13,11 @@ const SecOne = () => {
 
   return (
     <div>
-      <div className='mb-40 mt-12 md:mb-64 md:mt-24'>
+      <div className='mb-20 mt-8 md:mb-64 md:mt-24'>
         {visible && (
-          <div className='flex flex-col items-center m-4 md:m-10'>
+          <div className='flex flex-col items-center m-2 md:m-10'>
             <motion.h1
-              className='text-emerald-100 font-bold text-6xl md:text-8xl font-mono'
+              className='text-emerald-100 font-bold text-5xl sm:text-6xl md:text-8xl font-mono'
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 2 }}
@@ -25,7 +25,7 @@ const SecOne = () => {
               NAMASTE
             </motion.h1>
             <motion.h2
-              className='text-emerald-100 font-bold text-3xl md:text-5xl font-mono my-2'
+              className='text-emerald-100 font-bold text-2xl sm:text-3xl md:text-5xl font-mono my-2'
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 2, delay: 1 }}
@@ -33,7 +33,7 @@ const SecOne = () => {
               Hello
             </motion.h2>
             <motion.h2
-              className='text-red-200 font-light text-xl md:text-2xl font-mono animate-pulse'
+              className='text-red-200 font-light text-lg sm:text-xl md:text-2xl font-mono animate-pulse'
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 2, delay: 1 }}
