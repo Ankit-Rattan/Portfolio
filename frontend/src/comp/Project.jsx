7697ba33d22fc1d59c0233d74eb5cdae918fe197
Ankit <rattanankit2004@gmail.com>
2024-06-11 22:45:11 +0530

    Update Project

--- frontend/src/comp/Project.jsx
+++ frontend/src/comp/Project.jsx
@@ -15,14 +15,14 @@ const Project = () => {
                             <p className="mt-2 text-lime-200">Ankit Rattan</p>
                             <p className="mt-2 text-cyan-200">CBC: Create By Creators is a platform that provides you with a combination of two cutting-edge scopes together.</p>
                             <p className="mt-2 text-white">Tech Stack: ReactJS, Socketio, MongoDB, Express, TailwindCSS </p>
-                            <a href="https://github.com/Ankit-Rattan/EQ-Hacks-Submission" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                            <a href="https://devpost.com/software/cbc-p2gweu" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Devpost Hackathon Submission</a>
                         </div>
                         <div className="mt-8 w-[30rem]">
                             <h1 className="text-2xl font-semibold text-red-300">ThoughtBox</h1>
                             <p className="mt-2 text-lime-200">MERN+Socket Project</p>
                             <p className="mt-2 text-cyan-200">It is an open chat app for sharing your knowledge and thoughts with others, where you can also comment and any other person on their thoughts.</p>
                             <p className="mt-2 text-white">Tech Stack: Vite+Rect, Tailwind CSS, MongoAtlas, Express, Node, Socket</p>
-                            <a href="https://github.com/Ankit-Rattan/Chat_App-beta" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                            <a href="https://github.com/Ankit-Rattan/ThoughtBox---OpenChat" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
                         </div>
                         <div className="mt-8 w-[30rem]">
                             <h1 className="text-2xl font-semibold text-red-300">InfoLevel</h1>
@@ -35,8 +35,8 @@ const Project = () => {
                             <h1 className="text-2xl font-semibold text-red-300">Upvision - Tech Club of NIT Delhi</h1>
                             <p className="mt-2 text-lime-200">Official Technical Club Website</p>
                             <p className="mt-2 text-cyan-200">Upvision is the official Tech Club Website live at NIT Delhi official website.</p>
-                            <p className="mt-2 text-white">Tech Stack: ReactJS .  Tailwind</p>
-                            <a href="https://upvisionnitdelhi.netlify.app/" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                            <p className="mt-2 text-white">Tech Stack: ReactJS . Tailwind</p>
+                            <a href="https://upvisionnitdelhi.netlify.app/" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Website Link</a>
                         </div>
                     </div>
             </div>
