1717aac93150adbb123e3e9767324b9f00e69dab
Ankit <rattanankit2004@gmail.com>
2024-06-11 22:37:06 +0530

    fix update

--- frontend/src/comp/Project.jsx
+++ frontend/src/comp/Project.jsx
@@ -1,68 +1,47 @@
-
 import React, { useState, useEffect } from 'react'
-import { motion, useTransform, useViewportScroll } from 'framer-motion'
+import { motion, useTransform, useScroll } from 'framer-motion'
 
 const Project = () => {
-    const [isVisible, setIsVisible] = useState(false)
-
-    const { scrollY } = useViewportScroll()
-    const y = useTransform(scrollY, [0, 500], [0, 1])
-
-    useEffect(() => {
-        y.onChange(latest => setIsVisible(latest >= 1))
-    }, [y])
 
     return (
         <>
-            <div className=" m-5 i justify-center">
-                <motion.div
-                    className="opacity-0 transition duration-1000   p-8 shadow-sm shadow-slate-100"
-                    style={{ opacity: isVisible ? 1 : 0 }}
-                >
+            <div className="m-5 i justify-center">
+                   
                     <h1 className="text-center text-amber-100 mt-5 text-4xl font-semibold font-mono">Projects</h1>
-                <div className='flex flex-wrap justify-around'>
-
-
-
-                    <div className="mt-8 w-[30rem]">
-                        <h1 className="text-2xl font-semibold text-red-300">CBC</h1>
-                        <p className="mt-2 text-lime-200">MERN + Socketio Project</p>
-                        <p className="mt-2 text-lime-200">Ankit Rattan</p>
-                        <p className="mt-2 text-cyan-200">CBC: Create By Creators is a platform that provides you with a combination of two cutting-edge scopes together.</p>
-                        <p className="mt-2 text-white">Tech Stack : ReactJS, Socketio, MongoDB, Express, TailwindCSS </p>
-                        <a href="https://github.com/Ankit-Rattan/EQ-Hacks-Submission" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
-
+                    <div className='flex flex-wrap justify-around'>
+                        <div className="mt-8 w-[30rem]">
+                            <h1 className="text-2xl font-semibold text-red-300">CBC</h1>
+                            <p className="mt-2 text-lime-200">MERN + Socketio Project</p>
+                            <p className="mt-2 text-lime-200">Ankit Rattan</p>
+                            <p className="mt-2 text-cyan-200">CBC: Create By Creators is a platform that provides you with a combination of two cutting-edge scopes together.</p>
+                            <p className="mt-2 text-white">Tech Stack: ReactJS, Socketio, MongoDB, Express, TailwindCSS </p>
+                            <a href="https://github.com/Ankit-Rattan/EQ-Hacks-Submission" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                        </div>
+                        <div className="mt-8 w-[30rem]">
+                            <h1 className="text-2xl font-semibold text-red-300">ThoughtBox</h1>
+                            <p className="mt-2 text-lime-200">MERN+Socket Project</p>
+                            <p className="mt-2 text-cyan-200">It is an open chat app for sharing your knowledge and thoughts with others, where you can also comment and any other person on their thoughts.</p>
+                            <p className="mt-2 text-white">Tech Stack: Vite+Rect, Tailwind CSS, MongoAtlas, Express, Node, Socket</p>
+                            <a href="https://github.com/Ankit-Rattan/Chat_App-beta" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                        </div>
+                        <div className="mt-8 w-[30rem]">
+                            <h1 className="text-2xl font-semibold text-red-300">InfoLevel</h1>
+                            <p className="mt-2 text-lime-200">React App</p>
+                            <p className="mt-2 text-cyan-200">InfoLevel is general web-app for storing contact details like name and email, collecting real-time data.</p>
+                            <p className="mt-2 text-white">Tech Stack: Vite+React, Tailwind CSS, Firebase</p>
+                            <a href="https://github.com/Ankit-Rattan/InfoLevel_React" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                        </div>
+                        <div className="mt-8 w-[30rem]">
+                            <h1 className="text-2xl font-semibold text-red-300">Upvision - Tech Club of NIT Delhi</h1>
+                            <p className="mt-2 text-lime-200">Official Technical Club Website</p>
+                            <p className="mt-2 text-cyan-200">Upvision is the official Tech Club Website live at NIT Delhi official website.</p>
+                            <p className="mt-2 text-white">Tech Stack: ReactJS .  Tailwind</p>
+                            <a href="https://upvisionnitdelhi.netlify.app/" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
+                        </div>
                     </div>
-                    <div className="mt-8 w-[30rem]">
-                        <h1 className="text-2xl font-semibold text-red-300">ThoughtBox</h1>
-                        <p className="mt-2 text-lime-200">MERN+Socket Project</p>
-                        <p className="mt-2 text-cyan-200">It is an open chat app for sharing your knowledge and thoughts with others, where you can also comment and any other person on their thoughts.</p>
-                        <p className="mt-2 text-white">Tech Stack : Vite+Rect Tailwind CSS MongoAtlas Express Node  Socket</p>
-                        <a href="https://github.com/Ankit-Rattan/Chat_App-beta" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
-                    </div>
-
-                    <div className="mt-8 w-[30rem]">
-                        <h1 className="text-2xl font-semibold text-red-300">InfoLevel</h1>
-                        <p className="mt-2 text-lime-200">React App</p>
-                        <p className="mt-2 text-cyan-200">InfoLevel is general web-app for storing contact details like name and email, collecting real-time data.</p>
-                        <p className="mt-2 text-white">Tech Stack : Vite+React Tailwind CSS Firebase</p>
-                        <a href="https://github.com/Ankit-Rattan/InfoLevel_React" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
-                    </div>
-                    <div className="mt-8 w-[30rem]">
-                        <h1 className="text-2xl font-semibold text-red-300">Drum Kit</h1>
-                        <p className="mt-2 text-lime-200">JS Learning Project </p>
-                        <p className="mt-2 text-cyan-200">A general drum kit can be operated using keyboard</p>
-                        <p className="mt-2 text-white">Tech Stack : Javascript </p>
-                        <br/>
-                        <a href="https://github.com/Ankit-Rattan/InfoLevel_React" className="mt-4 block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Github Repository</a>
-                    </div>
-                </div>
-                </motion.div>
             </div>
         </>
     )
 }
 
-export default Project;
-
-
+export default Project
--- frontend/src/comp/SecTwo.jsx
+++ frontend/src/comp/SecTwo.jsx
@@ -18,16 +18,24 @@ const SecTwo = () => {
  }, []);
 
  return (
-    <div className="mt-16">
-      <div className="container mx-auto px-4 py-8 ">
+    <div className="mt-3">
+      <div className="container mx-auto px-4 py-5 ">
         <h1 className="text-white text-2xl font-extralight mb-4 font-mono m-3 text-center">
           <span ref={typedRef}></span>
         </h1>
         <div className='flex flex-wrap justify-around items-center'>
+        <div className='m-2'>
           <a href='https://leetcode.com/rattanankit2004/' className='text-orange-300 text-2xl font-mono mx-4 transition duration-500 ease-in-out transform hover:scale-110  hover:text-orange-400 '>LeetCode<br/><p className='text-xl text-orange-200'>300+ Ques</p> </a>
+        </div>
+        <div className='m-2'>
           <a href='https://www.codechef.com/users/ankitrattan' className='text-orange-300 text-2xl font-mono mx-4 transition duration-500 ease-in-out transform hover:scale-110 hover:text-orange-400'>Codechef <br/><p className='text-xl text-orange-200'>Max. 1457</p></a>
+        </div>
+        <div className='m-2'>
           <a href='https://github.com/Ankit-Rattan' className='text-orange-300 text-2xl font-mono mx-4 transition duration-500 ease-in-out transform hover:scale-110 hover:text-orange-400'>GitHub <br/>  .👨‍💻.</a>
+        </div>
+        <div className='m-2'>
           <a href='https://auth.geeksforgeeks.org/user/rattananul1v' className='text-orange-300 text-2xl font-mono mx-4 transition duration-500 ease-in-out transform hover:scale-110 hover:text-orange-400'>GfG<br/><p className='text-xl text-orange-200'>#19</p></a>
+        </div>
         </div>
 
       </div>
