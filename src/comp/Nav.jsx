4db20db1596ae2048b0fc9d383550332dd1344b5
Ankit <rattanankit2004@gmail.com>
2025-05-25 14:50:58 +0530

    added resume

--- src/comp/Nav.jsx
+++ src/comp/Nav.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>
-      <p className='text-white font-mono text-xl m-3 animate-pulse'> Ankit Rattan </p>
+    <div className="flex justify-between items-center p-4 bg-transparent">
+      <p className="text-white font-mono text-xl animate-pulse">Ankit Rattan</p>
+      
+      <a
+        href="https://drive.google.com/file/d/1JW4DZzmOiKKrf_Kp19bdnsGDoUqzDhwC/view?usp=sharing"
+        download
+        className="bg-slate-400 text-black px-4 py-2 rounded-md font-semibold hover:bg-gray-200 transition-all"
+      >
+        Resume
+      </a>
     </div>
   )
 }
